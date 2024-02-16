6a74a128087553d09d31d8e2954e5e1c355e253f
Gabriel Moreira Mattos <gabrielmoreirailustra@gmail.com>
2024-02-16 12:25:53 -0300

    feat: add style config file

---
+++ components/configs.ts
@@ -0,0 +1,25 @@
+export const config = {
+  fontSizes: {
+    lg: 24,
+    md: 18,
+    sm: 16,
+  },
+  spaces: {
+    lg: 24,
+    md: 16,
+    sm: 12,
+    xs: 8,
+  },
+  radius: {
+    lg: 24,
+    sm: 12,
+  },
+  colors: {
+    Primary: "#4b66d2",
+    Secoundary: "#414f6d",
+    background: "#f8f8ff",
+    complementary_1: "#d2d4e9",
+    complementary_2: "#707680",
+    something_bad: "#ff4b4b",
+  },
+} as const;
